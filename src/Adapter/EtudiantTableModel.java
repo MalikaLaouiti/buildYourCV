0e096da35e7eee9ad8c383122d04fbbc22f0228a
MalikaLaouiti <laouiti.malika@yahoo.com>
2026-02-24 11:24:40 +0100

    implementation de model pour etudaints

--- src/Adapter/EtudiantTableModel.java
+++ src/Adapter/EtudiantTableModel.java
@@ -1,4 +1,20 @@
 package Adapter;
 
-public class EtudiantTableModel {
+import javax.swing.table.AbstractTableModel;
+
+public class EtudiantTableModel extends AbstractTableModel {
+    @Override
+    public int getRowCount() {
+        return 0;
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        return null;
+    }
 }
