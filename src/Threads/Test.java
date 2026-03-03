5979f839bbe8902500eff0cf3d8a7e3f7ebda146
MalikaLaouiti <laouiti.malika@yahoo.com>
2026-03-03 11:48:14 +0100

    join: attent le thread jusqu'à il mort

--- src/Threads/Test.java
+++ src/Threads/Test.java
@@ -2,11 +2,22 @@ package Threads;
 
 public class Test {
     public static void main(String[] args) {
+        //1 thread par defaut pour main
         System.out.println("Debut PP");
         Traitement ta=new Traitement("A");
         Traitement tb=new Traitement("B");
-        ta.start();
+
+        //creation des thread par start(): 3 thread
+        ta.start();//run charge programme dans pile , start cree une pile et charge run pour executer
         tb.start();
+        if (ta.isAlive()){
+            try {
+                ta.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println("Fin PP");
+        //mort de main et fonction A B fonctionne en arriere plan : 2 threads
     }
 }
